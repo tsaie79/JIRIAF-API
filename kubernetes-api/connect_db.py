b07408e2dd11dd1bd612ddc04c3742e1afd24f14
tsaie79 <tsaie79@gmail.com>
2023-10-12 20:15:28 +0000

    update

---
+++ kubernetes-api/connect_db.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+# establish a connection to the database with port 37082
+cnx = mysql.connector.connect(user='jiriaf', password='dikala-twiga', host='127.0.0.1', port='3306', database='jiriaf')
+
+# create a cursor object
+cursor = cnx.cursor()
+
+# execute the query print out the tables in the database jiriaf
+
+cursor.execute("SHOW TABLES")
+for (table_name,) in cursor:
+    print(table_name)
+
+# close the cursor object
+cursor.close()
+
+# close the connection
+cnx.close()
+
