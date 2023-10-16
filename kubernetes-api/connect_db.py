38f95ff6360dbf2a968decdeb3c18cc1585cea36
tsaie79 <tsaie79@gmail.com>
2023-10-16 18:46:51 +0000

    fix the port

--- kubernetes-api/connect_db.py
+++ kubernetes-api/connect_db.py
@@ -1,8 +1,7 @@
 import mysql.connector
 
-# establish a connection to the database with port 37082
-## ssh -NfL 12345:localhost:37082 jiriaf2301
-cnx = mysql.connector.connect(user='jiriaf', password='dikala-twiga', host='127.0.0.1', port='12345', database='jiriaf')
+# establish a connection to the database with port 3306
+cnx = mysql.connector.connect(user='jiriaf', password='dikala-twiga', host='127.0.0.1', port='3306', database='jiriaf')
 
 # create a cursor object
 cursor = cnx.cursor()
